a2d7aacaaa61dafbb51962c1f0a2dcf5b95cf0d1
hcc007 <wanghaichao2016@gmail.com>
2020-03-03 19:32:32 +0800

    ui 控件学习

--- app/src/main/java/com/hc/jetpackdemo/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/hc/jetpackdemo/ui/dashboard/DashboardFragment.java
@@ -18,18 +18,25 @@ public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
 
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+
+        View root = inflater.inflate(R.layout.layout_ui_widget, container, false);
+
+
+//        final TextView textView = root.findViewById(R.id.text_dashboard);
+//        dashboardViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+//
+
         return root;
     }
 }
